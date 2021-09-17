cca477ce8c5a98e9b36a7bf34a95436c0f0a16e3
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2021-09-16 20:21:01 -0500

    Reworked the header section

--- lib/Doctool/Getline.pm
+++ lib/Doctool/Getline.pm
@@ -4,6 +4,52 @@ use 5.006;
 use strict;
 use warnings;
 
+BEGIN {
+    require Exporter;
+    our $VERSION = v0.1;
+    our @ISA = qw(Exporter);
+    our @EXPORT = qw(nextline prevline getline);
+}
 
+# Usage: _nextline string index
+sub nextline($\$) {
+    use Doctool::Util 'min';
+    my ($string,$index) = @_;
+    my $end = length($string) - 1;
+    my $i = index($string, "\n", $$index);
+    if ($i < 0) {
+        $$index = $end;
+    } else {
+        $$index = min($i + 1, $end);
+    }
+}
 
-1; # End of Doctool::Getline
+# Usage: _prevline string index
+sub prevline ($\$) {
+    my ($string,$index) = @_;
+    my $i = rindex($string, "\n", $$index);
+    if (abs($i - $$index) == 1) {
+        $i = rindex($string, "\n", $$index - 2);
+    }
+    $$index = $i + 1;
+}
+
+# Usage: _getline string index
+sub getline ($\$) {
+    # $start is a reference to a scalar
+
+    # String and starting index
+    my ($string,$start) = @_;
+
+    # End of line (or end of string in case of no newline)
+    my $end = index($string, "\n", $$start);
+    if ($end < 0) {
+        $end = length($string);
+    }
+
+    my $result = substr($string, $$start, $end - $$start);
+    $$start = $end + 1; # Advance index to the next line
+    return $result;
+}
+
+1;
--- lib/Doctool/Util.pm
+++ lib/Doctool/Util.pm
@@ -9,9 +9,9 @@ use Carp;
 # ABSTRACT: Utility subroutines for Doctool
 
 our @ISA = qw(Exporter);
-our @EXPORT = qw(append echo funcref max min printarray printhash str);
-our @EXPORT_OK = ();
-our @EXPORT_TAGS = ('all' => [@EXPORT, @EXPORT_OK]);
+#our @EXPORT = qw();
+our @EXPORT_OK = qw(append echo funcref max min printarray printhash push_back str);
+#our @EXPORT_TAGS = ('all' => [@EXPORT, @EXPORT_OK]);
 our $VERSION = v0.10;
 
 # Usage: append array/scalar $value
@@ -26,6 +26,17 @@ sub append (\[@$]$) {
     } elsif ($type eq 'SCALAR') {
         $$ref = $$ref . $value;
     }
+
+    return $ref;
+}
+
+# push_back ARRAY, VALUE
+sub push_back (\@$) {
+    my $ref = shift;
+    my $value = shift;
+
+    my $count = scalar @$ref;
+    $ref->[$count] = $value;
 
     return $ref;
 }
@@ -47,10 +58,10 @@ sub funcref {
 
 # Print the contents of an array
 sub printarray {
-    croak _invalid_call('printarray', 'array|arrayref') if scalar(@_) == 0;
+    croak _invalid_call('printarray', 'LIST|ARRAYREF') if scalar(@_) == 0;
 
     if (ref $_[0] eq 'ARRAY') {
-        croak _invalid_call('printarray', 'array|arrayref') if scalar(@_) != 1;
+        croak _invalid_call('printarray', 'LIST|ARRAYREF') if scalar(@_) != 1;
 
         my $ar = shift; # Reference
         echo $ar;
@@ -68,7 +79,7 @@ sub printarray {
 
 # Print the contents of a hash
 sub printhash {
-    croak _invalid_call('printhash', 'hash|hashref') if scalar(@_) != 1;
+    croak _invalid_call('printhash', 'HASH|HASHREF') if scalar(@_) != 1;
 
     if (ref $_[0] eq 'HASH') {
         my $hash = shift; # Is reference
@@ -179,69 +190,3 @@ Prints the contents of a hash. The parameter can be any valid hash or hash refer
 =item str( arg, ... )
 
 Concatenates the arguments into a string. Requires at least one argument.
-
-=back
-
-=head1 AUTHOR
-
-John Russell, C<< <john at cpan.org> >>
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Doctool-Util>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Doctool-Util>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Doctool-Util>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Doctool-Util/>
-
-=back
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2021 John Russell.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of the the Artistic License (2.0). You may obtain a
-copy of the full license at:
-
-L<http://www.perlfoundation.org/artistic_license_2_0>
-
-Any use, modification, and distribution of the Standard or Modified
-Versions is governed by this Artistic License. By using, modifying or
-distributing the Package, you accept this license. Do not use, modify,
-or distribute the Package, if you do not accept this license.
-
-If your Modified Version has been derived from a Modified Version made
-by someone other than you, you are nevertheless required to ensure that
-your Modified Version complies with the requirements of this license.
-
-This license does not grant you the right to use any trademark, service
-mark, tradename, or logo of the Copyright Holder.
-
-This license includes the non-exclusive, worldwide, free-of-charge
-patent license to make, have made, use, offer to sell, sell, import and
-otherwise transfer the Package with respect to any patent claims
-licensable by the Copyright Holder that are necessarily infringed by the
-Package. If you institute patent litigation (including a cross-claim or
-counterclaim) against any party alleging that the Package constitutes
-direct or contributory patent infringement, then this Artistic License
-to you shall terminate on the date that such litigation is filed.
-
-Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
-AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
-THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
-YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
-CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
-CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
