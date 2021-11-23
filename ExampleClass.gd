dfa7c089d4a946ae2b4612a094f4a7c6f529f32b
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2021-11-23 10:29:11 -0600

    Added example script

--- ExampleClass.gd
+++ ExampleClass.gd
@@ -1,5 +1,53 @@
 ## A stripped version of @class Node.
 # @desc This is an example class for testing Doctool's capabilities. It is based on @a Node,
 #       but it does not feature everything that the original has.
+#
+#       The following functions are imported from @class Node:
+#
+#       @list_begin{unordered}
+#       @item _enter_tree
+#       The @function _enter_tree() function is supported.
+#       @item _exit_tree
+#       The @function _exit_tree() function is supported.
+#       @list_end
+#
+#       Imported signals:
+#
+#       @list_begin{unordered}
+#       @item ready
+#       The ready signal is supported.
+#       @item renamed
+#       The renamed signal is supported.
+#       @list_end
 extends Object
 class_name NodeCustom
+
+## Emitted when a node is ready.
+signal ready
+
+## Emitted when a node is renamed.
+signal renamed
+
+## The name of the node.
+# @type String
+# @setter set_name(name)
+# @getter get_name()
+# @desc This name is unique among siblings of a node. If set to a name that already exists,
+#       it is automatically renamed.
+#
+#       @b Note: auto-generated names can contain a '@' character, which is reserved for unique names
+#       when adding a node with @function add_child(). When setting the name manually, all instances
+#       of '@' will be removed.
+var name : String = ""
+
+## Called when a @class Node enters the scene tree.
+# @virtual
+# @desc If the node has children, the @function _enter_tree() of those children will be called
+#       after this one has finished.
+func _enter_tree() -> void: pass
+
+## Called when a @class Node exits the scene tree.
+# @virtual
+# @desc If the node has children, the @function _exit_tree() of those children will be called
+#       first, and this one last.
+func _exit_tree() -> void: pass
