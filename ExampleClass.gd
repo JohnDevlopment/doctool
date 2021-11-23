dd61fd83cf4a463066b2e1aad45b5ae4a44f9230
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2021-11-23 05:15:02 -0600

    Add example class

---
+++ ExampleClass.gd
@@ -0,0 +1,5 @@
+## A stripped version of @class Node.
+# @desc This is an example class for testing Doctool's capabilities. It is based on @a Node,
+#       but it does not feature everything that the original has.
+extends Object
+class_name NodeCustom
